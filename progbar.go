e45ea07d3670ff08ca29dac7661204b2b7c97c9f
Minu Mihail HRISTODOR <mhristodor@bitdefender.com>
2024-03-12 18:36:04 +0200

    fixed identation

--- progbar.go
+++ progbar.go
@@ -8,22 +8,22 @@ import (
 
 
 func GetProgressBar(size int64, descr string, padding string) *progressbar.ProgressBar {
-	return progressbar.NewOptions64(size, 
-		progressbar.OptionEnableColorCodes(true), 
-		progressbar.OptionShowBytes(true), 
-		progressbar.OptionShowCount(), 
-		progressbar.OptionShowElapsedTimeOnFinish(), 
-		progressbar.OptionSetVisibility(true), 
-		progressbar.OptionSetElapsedTime(true),
-		progressbar.OptionOnCompletion(func() {
-			fmt.Printf("\n")
-		}),
-		progressbar.OptionSetDescription(descr + padding),
-		progressbar.OptionSetTheme(progressbar.Theme{
-			Saucer:        "[green]=[reset]",
-			SaucerHead:    "[green]>[reset]",
-			SaucerPadding: " ",
-			BarStart:       "[green]|",
-			BarEnd:       "[green]|[reset]",
-		}))
+    return progressbar.NewOptions64(size, 
+        progressbar.OptionEnableColorCodes(true), 
+        progressbar.OptionShowBytes(true), 
+        progressbar.OptionShowCount(), 
+        progressbar.OptionShowElapsedTimeOnFinish(), 
+        progressbar.OptionSetVisibility(true), 
+        progressbar.OptionSetElapsedTime(true),
+        progressbar.OptionOnCompletion(func() {
+            fmt.Printf("\n")
+        }),
+        progressbar.OptionSetDescription(descr + padding),
+        progressbar.OptionSetTheme(progressbar.Theme{
+            Saucer:        "[green]=[reset]",
+            SaucerHead:    "[green]>[reset]",
+            SaucerPadding: " ",
+            BarStart:       "[green]|",
+            BarEnd:       "[green]|[reset]",
+        }))
 }
